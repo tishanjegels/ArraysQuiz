13f188dff9aad8ff297519f1810c099e96dbf19a
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-05-15 13:08:17 +0200

    Add files via upload

---
+++ js/quizGame.js
@@ -0,0 +1,93 @@
+// initialize empty array
+let correctAnswers = [];
+
+// initialize array with letter that repesent correct answer
+
+let letters = ['a','b','c','d'];
+
+// create questions for quiz
+
+let question1 = prompt("=== Only enter alaphabet ===\n What is our national rugby teams name? \n a. All Blacks \n b. Springboks \n c. Stormers \n d. Blitzboks ");
+let question2 = prompt("=== Only enter alaphabet ===\n Who won the 2019 Rugby World Cup ? \n a. Springboks \n b. England \n c. All Blacks  \n d. USA ");
+let question3 = prompt("=== Only enter alaphabet ===\n Who won the 7's rugby series in 2021 ? \n a. Fiji \n b. New Zealand \n c. USA \n d. Blitzboks ");
+let question4 = prompt("=== Only enter alaphabet ===\n How many player are there in a rugby team?  \n a. 7 \n b. 25 \n c. 15 \n d. 23 ");
+let question5 = prompt("=== Only enter alaphabet ===\n Who won the 20/21 Premier League ? \n a. Liverpool \n b. Man City \n c. Arsenal \n d. Man United ");
+let question6 = prompt("=== Only enter alaphabet ===\n Who won Champions League 2021? \n a. PSG \n b. Chelsea \n c. Real Madrid  \n d. Man City ");
+let question7 = prompt("=== Only enter alaphabet ===\n Which team does Christiano Ronaldo Play for?  \n a. Real Madrid \n b. Juventus \n c. Man City \n d. Man United ");
+let question8 = prompt("=== Only enter alaphabet ===\n In which team are the following player: Jadon Sancho ,Tammy Abraham ,Jude Bellingham ? \n a. England \n b. France \n c.  Portugal \n d. Italy ");
+
+// if statement to check answers
+
+if (question1 === "b"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    }
+// Question 2 //
+
+if (question2 === "a"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    }
+// Question 3 //
+
+if (question3 === "d"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    } 
+// Question 4 //
+
+if (question4 === "c"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    }
+// Question 5 //
+
+if (question5 === "b"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    } 
+// Question 6//
+
+if (question6 === "b"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    } 
+// Question 7 //   
+
+if (question7 === "d"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    } 
+// Question 8 //
+
+if (question8 === "a"){
+    correctAnswers.push();
+    document.write("<p>Correct!!<p>");
+}
+    else{
+        document.write("<p>Incorrect!!<p>");
+    } 
+
+// Get mark at end of quiz 
+
